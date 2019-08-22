d7ac0d13bc62db60ca7b9440820e9c309780bb93
kvartik <kvartik0606@gmail.com>
2019-08-22 21:27:49 +0300

    validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
 #attr_accessible  :price, :name, :real, :weight, :description
 validates :price, { numericality: {greater_than: 0, allow_nill: true} }
 validates :name, :description, presence: true
+
+after_initialize { puts "initialized" }# Item.new; Item.first
+
+after_save { puts "saved" } #Item.save; Item.create
+after_create { puts "created" }
+after_update { puts "updated" }
+after_destroy { puts "destroyed" }# item.destroy
 end
