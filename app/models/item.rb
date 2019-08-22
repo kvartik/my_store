a0201ce2b7e327692fd27b6e7466452437b2671d
kvartik <kvartik0606@gmail.com>
2019-08-22 21:00:12 +0300

    validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
 #attr_accessible  :price, :name, :real, :weight, :description
+validates :price, { numericality: {greater_than: 0, allow_nill: true} }
+validates :name, :description, presence: true
 end
